bfa1dc1b1b866f5082c8b5752a4c94bf758f0480
Abimanyu Darmawan <abimanyudarmawan4@gmail.com>
2023-01-05 00:21:05 +0700

    Initial Commit

---
+++ Contents/mods/WartegFoodExtended/media/lua/server/WFE_recipecodes.lua
@@ -0,0 +1,18 @@
+function recipe_ReturnSaucepan(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("Base.Saucepan");
+end
+
+function Recipe.OnCreate.WFE_MakeBowlIndomie(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("Base.Saucepan");
+	result:setCooked(true);
+	result:setHeat(2.5);
+end
+
+function Recipe.OnCreate.WFE_MakeBowlMieSedaap(items, result, player)
+	local inv = player:getInventory();
+	inv:AddItem("Base.Saucepan");
+	result:setCooked(true);
+	result:setHeat(2.5);
+end
